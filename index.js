3c8c4475adf1c67881abb4d8ef5678aa3f2d840c
Mazin <mazin.sherif100@gmail.com>
2022-04-30 17:25:39 +0200

    make it better

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get('/', (req, res)=>{
 })
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
   
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true}) 
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true}) 
     .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(`error: ${error}`));
 
